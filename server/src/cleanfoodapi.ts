d6456e8f19af7944b7003cfe094d535a64e634b5
Jeong-TaekOh <jeongtaek0128@gmail.com>
2023-06-30 18:01:42 +0900

    fixed untracked files

--- server/src/cleanfoodapi.ts
+++ server/src/cleanfoodapi.ts
@@ -2,12 +2,21 @@ import { RESTDataSource } from "@apollo/datasource-rest";
 
 
 export default class CleanFoodAPI extends RESTDataSource {
-    override baseURL = `https://openapi.foodsafetykorea.go.kr/api`;
+    override baseURL = "https://openapi.foodsafetykorea.go.kr/api/";
     
     async getDirtyFoodCompList(id: string){
-      console.log(process.env.FoodSafetyGOVENV)
-      console.log('here')
-      return this.get(`/${process.env.FoodSafetyGOVENV}/I1850/xml/0/1000/CHNG_DT=20220629`);
+      const response = await this.get(`${process.env.FoodSafetyGOVENV}/I1850/xml/0/1000/CHNG_DT=20220629`);
+      return this.getDirtyFoodCompReducer(response)
     }
 
+    getDirtyFoodCompReducer(post: any){
+      post = post.I1850.row;
+      return {
+        name: post.ENTRPS_NM,
+        expire_date: post.PUBLC_BGN_DT,
+        details: post.VILT_DTLS,
+        address: post.ENTRPS_BASS_ADDR,
+        business_type: post.BSN_KND_NM,
+      }
+    }
   }
