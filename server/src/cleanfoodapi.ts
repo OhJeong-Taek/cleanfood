7c5f0b99c9b8be11edf99dbb013da9a8969d85d6
Jeong-TaekOh <jeongtaek0128@gmail.com>
2023-06-30 20:04:50 +0900

    data loading success

--- server/src/cleanfoodapi.ts
+++ server/src/cleanfoodapi.ts
@@ -1,22 +1,23 @@
 import { RESTDataSource } from "@apollo/datasource-rest";
-
+import { parseStringPromise } from 'xml2js'
 
 export default class CleanFoodAPI extends RESTDataSource {
     override baseURL = "https://openapi.foodsafetykorea.go.kr/api/";
     
     async getDirtyFoodCompList(id: string){
       const response = await this.get(`${process.env.FoodSafetyGOVENV}/I1850/xml/0/1000/CHNG_DT=20220629`);
-      return this.getDirtyFoodCompReducer(response)
+      const result = await parseStringPromise(response);
+      return result.I1850.row.map((post: any) => this.getDirtyFoodCompReducer(post));
     }
 
     getDirtyFoodCompReducer(post: any){
-      post = post.I1850.row;
       return {
-        name: post.ENTRPS_NM,
-        expire_date: post.PUBLC_BGN_DT,
-        details: post.VILT_DTLS,
-        address: post.ENTRPS_BASS_ADDR,
-        business_type: post.BSN_KND_NM,
+        id: post.$.id,
+        name: post.ENTRPS_NM[0],
+        expire_date: post.PUBLC_BGN_DT[0],
+        details: post.VILT_DTLS[0],
+        address: post.ENTRPS_BASS_ADDR[0],
+        business_type: post.BSN_KND_NM[0],
       }
     }
   }
--- server/src/schema.ts
+++ server/src/schema.ts
@@ -5,6 +5,7 @@ export const typeDefs = `#graphql
   # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
 
   type CompInfo {
+    id: ID!
     name: String
     expire_date: String
     details: String
